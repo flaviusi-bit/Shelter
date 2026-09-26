8c5d934e28798ee8498be90f6efd4ec3025880db
flaviusi-bit <flaviusi@gmail.com>
2026-09-26 12:29:44 +0300

    Schedule medical reminder reconciliation

--- backend/src/main/java/com/shelter/api/task/TaskReminderService.java
+++ backend/src/main/java/com/shelter/api/task/TaskReminderService.java
@@ -6,6 +6,7 @@ import com.shelter.api.medical.DewormingRepository;
 import com.shelter.api.medical.Vaccination;
 import com.shelter.api.medical.VaccinationRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -27,6 +28,11 @@ public class TaskReminderService {
         this.vaccinations = vaccinations;
         this.dewormings = dewormings;
         this.zone = ZoneId.of(timezone);
+    }
+
+    @Scheduled(fixedDelayString = "${shelter.medical-reminder-sync-ms:3600000}")
+    public void scheduledSync() {
+        syncAll();
     }
 
     public int syncAll() {
